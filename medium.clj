f3f3a02ea028e06231c465ce3fc4a466fdffa5aa
Ryan Abdurohman <ryanabdurohman81@gmail.com>
2022-08-19 09:06:16 +0700

    update medium 18 aug

--- medium.clj
+++ medium.clj
@@ -50,3 +50,79 @@
       lst
       (recur (conj lst (take n a)) (drop n a)))))
 ;; ==============
+;;
+;; 18 Aug 22
+;;
+;; ==============
+;; > To Tree, or not to Tree
+;;   problem/95
+;; solution 1
+(fn [x]
+  (->> (flatten x)
+       (map #(if (not (nil? %)) 1 0))
+       (apply +)
+       (#(- (- (count (flatten x)) %) %))
+       (= 1)))
+;; solution 2
+(fn f [t]
+  (or (nil? t)
+      (and (coll? t)
+           (= 3 (count t))
+           (every? f (rest t)))))
+;; ==============
+;; > Recognize Playing Cards
+;;   problem/128
+;; solution 1
+(fn [x]
+  (let [kind {"S" :spade, "H" :heart, "D" :diamond, "C" :club}
+        rank-type [2 3 4 5 6 7 8 9 "T" "J" "Q" "K" "A"]
+        rank (into (sorted-map) (map-indexed (fn [idx itm] [(str itm) idx]) rank-type))]
+    {:suit (kind (str (get x 0))) :rank (rank (str (get x 1)))}))
+;; solution 2
+(fn [[a b]]
+  (let [s (zipmap "DHCS" [:diamond :heart :club :spade])
+        r (zipmap "23456789TJQKA" (range 13))]
+    {:suit (s a) :rank (r b)}))
+;; ==============
+;; > Pairwise Disjoint Sets
+;;   problem/153
+;; solution 1
+(fn [x]
+  (let [lst (into [] x)]
+    (->> (for [i lst]
+           (->> (map #(if (not (= i %)) (= i (apply disj i %)) nil) lst)
+                (filter #(not (nil? %)))))
+         flatten
+         (every? true?))))
+;; solution 2
+(fn [sets]
+  (= (reduce + (map count sets))
+     (count #_{:clj-kondo/ignore [:unresolved-namespace]}
+            (reduce clojure.set/union sets))))
+;; ==============
+;; > Beauty is Symmetry
+;;   problem/96
+;; solution 1
+(fn [y]
+  (let [get-tree (fn [x] (->> (tree-seq coll? seq x)
+                              rest
+                              (filter coll?)
+                              (take-last 2)))
+        left (second y)
+        right (last y)
+        valid (not (or (nil? left) (nil? right)))]
+    (if valid 
+      (or (= (get-tree left) (reverse (get-tree right)))
+          (= left right))
+      false)))
+;; solution 2
+#(= ((fn mirror [[n l r :as tree]] (when tree [n (mirror r) (mirror l)])) %) %)
+;; ==============
+;; > Count Occurences
+;;   problem/55
+;; solution 1
+(fn [x]
+  (->> (group-by identity x)
+       (map (fn [k] [(key k) (count (val k))]))
+       (into {})))
+;; ==============
